64220bc05a0bf8fdbf16beadcc80db700bf4900f
Ilya Kolesnikov <ivkol@mail.ru>
2022-02-03 13:21:23 +0300

    master - example controller and couple of service interfaces

---
+++ app/Interfaces/EmployeeServiceInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Interfaces;
+
+interface EmployeeServiceInterface
+{
+    public function create();
+
+    public function edit();
+
+    public function remove();
+}
---
+++ app/Interfaces/PositionServiceInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Interfaces;
+
+interface PositionServiceInterface
+{
+    public function create();
+
+    public function edit();
+
+    public function remove();
+}
