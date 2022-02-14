13b3feb167e0adcf2f0e90ba65ec300c71f7459a
Ilya Kolesnikov <ivkol@mail.ru>
2022-02-14 12:43:43 +0300

    master - migration for m-t-m between Employees and Departments

---
+++ database/migrations/2022_02_13_074351_create_employees_departments.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesDepartments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees_departments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('employee_id');
+            $table->integer('department_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees_departments');
+    }
+}
